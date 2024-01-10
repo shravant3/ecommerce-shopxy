b5569df44c4adf9ef55976a369f6d1dfa5e484c1
shravant3 <shravant3@gmail.com>
2024-01-11 00:03:43 +0530

    updatess

--- src/test/java/com/shopxy/ecom/EcomApplicationTests.java
+++ src/test/java/com/shopxy/ecom/EcomApplicationTests.java
@@ -3,7 +3,7 @@ package com.shopxy.ecom;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class EcomApplicationTests {
 
 	@Test
